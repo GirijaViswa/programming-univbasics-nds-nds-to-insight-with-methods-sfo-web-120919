1b00cb9c24a459d1100330e8c479fbdaa64e0805
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-07 23:35:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,7 @@ def gross_for_director(director_data)
  # pp director_data[:movies]
   i = 0
   while i < director_data[:movies].length do 
-    pp director_data[:movies][i][:worldwide_grosses]
+    pp director_data[:movies][i][:worldwide_gross]
     i += 1
   end
   
